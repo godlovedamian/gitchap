5884f5763bdd322b4880744391dfc14d5bcbf8e4
Godlove Damian <godlove@innocent.com>
2019-10-12 19:51:27 +0300

    feat: refactored code to inquire github login credentials

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+/**
+ * Description: Entry point to gitchap.
+ * Author: Godlove Damian.
+ * Created: October 2019.
+ */
+
 'use strict';
 
 //npm modules setup
+const gitchap = require('commander');
+
 const chalk = require('chalk');
 const figlet = require('figlet');
 const clear = require('clear');
+
+//self modules setup
+const files = require('./lib/files');
+
+gitchap
+    .command('init')
+
+    .description('draw app banner')
+
+    .action(() => {
+        clear();
+        console.log(chalk.magenta(figlet.textSync('gitchap', {horizontalLayout: 'full'})));
+    });
+
+gitchap.parse(process.argv)
+if (!gitchap.args.length){
+    gitchap.help();
+}
+// (gitchap.args.length) ? gitchap.help() : "";
