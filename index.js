e250cd93820db036d292a3c99fb33f5c3e09741a
Godlove Damian <godlove@innocent.com>
2019-10-12 23:57:44 +0300

    feat: added create_repo command, handled different errors from Github auth API

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const clear = require('clear');
 //self modules setup
 const files = require('./lib/files');
 const github = require('./lib/github_credentials');
+const inquirer = require('./lib/inquirer');
+const repo = require('./lib/create_a_repo');
 
 gitchap
     .command('init')
@@ -24,6 +26,49 @@ gitchap
         clear();
         console.log(chalk.magenta(figlet.textSync('gitchap', {horizontalLayout: 'full'})));
     });
+
+gitchap
+    .command('create_repo')
+    .description('Creates a new repository on Github')
+    .action(async () => {
+        const getGithubToken = async () => {
+            let token = github.getStoredGithubToken();
+            if (token) {
+                return token;
+            }
+            await github.setGithubCredentials();
+            token = await github.registerNewToken();
+            return token;
+        }
+        try {
+            const token = await getGithubToken();
+            github.gitHubAuth(token);
+
+            const url = await repo.createRemoteRepository();
+
+            await repo.createGitIgnore();
+
+            const complete = await repo.setupRepository(url);
+
+            if (complete) {
+                console.log(chalk.green('Repository was successfully created'));
+            }
+        } catch (error) {
+            if (error) {
+                switch (error.status) {
+                    case 401:
+                        console.log(chalk.red('Couldn\'t log in, Please provide the correct credentials'))
+                        break;
+                    case 422:
+                        console.log(chalk.red('There already exist a remote repository with the same name'))
+                        break;
+                    default:
+                        console.log(chalk.red(error))
+                        break;
+                }
+            }
+        }
+    })
 
 gitchap
     .command('octocheck')
